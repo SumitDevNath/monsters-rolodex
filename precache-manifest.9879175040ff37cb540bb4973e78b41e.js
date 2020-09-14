3dc40ff76a746e27672eb87d42962bcf6b245668
unknown <sumit.devnath1@gmail.com>
2020-09-14 20:44:55 +0600

    Updates

---
+++ precache-manifest.9879175040ff37cb540bb4973e78b41e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69a3ee8d2e0a7fb9e5b3d9c1a92b0272",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f24988f3a632da3bc9a8",
+    "url": "/monsters-rolodex/static/css/main.9ab5a904.chunk.css"
+  },
+  {
+    "revision": "4a43f021320c20e275a9",
+    "url": "/monsters-rolodex/static/js/2.fcf6991a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.fcf6991a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f24988f3a632da3bc9a8",
+    "url": "/monsters-rolodex/static/js/main.1dce1e93.chunk.js"
+  },
+  {
+    "revision": "dfba82531f669c6858b0",
+    "url": "/monsters-rolodex/static/js/runtime-main.589ddcef.js"
+  }
+]);
